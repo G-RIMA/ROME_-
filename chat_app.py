2ccbd956e86662445e6602087a670d9748a2dc4b
G-RIMA <gikandimaria178@gmail.com>
2022-12-03 12:28:01 +0300

    Created a chatbot with a UI

---
+++ chat_app.py
@@ -0,0 +1,35 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Creating ChatBot Instance
+chatbot = ChatBot(
+    'Rome',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+    
+    )
+
+training_data_quesans = open('/home/gamal/ROME/training_data/QnA.txt').read().splitlines()
+training_data_personal = open('/home/gamal/ROME/training_data/QnA1.txt').read().splitlines()
+
+training_data = training_data_quesans + training_data_personal
+
+trainer = ListTrainer(chatbot)
+trainer.train(training_data) 
+
+# Training with English Corpus Data 
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+trainer_corpus.train(
+    'chatterbot.corpus.english')
+    
+
+
+
