7f40a6e03a1a181d42aaf1c46094bd7d61ab4454
grima <gikandimaria178@gmail.com>
2022-12-13 11:06:41 +0300

    added a button and information about the chatbot

---
+++ landing_page/src/Component/Home/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h2> A chatbot for you when you need a friend!</h2>
+                        <p>Rome is a simple chatbot that you can chat with or play with.It is a simple virtual pet that
+                            you can interact with when you are sad, happy or just need some company.
+                        </p>
+                        <form>
+                            <button type="button">Chat with Rome Now</button>
+                        </form>
+                        
+                    </div>
+                </div>
+                </div> 
+        </header>
+    )
+}
+
+export default Header;
