84533bc11cf74b9b0f1bea43bf8c6056e9d3cf60
grima <gikandimaria178@gmail.com>
2022-12-14 16:49:02 +0300

    added a background image

--- landing_page/src/Component/Home/Header/Header.js
+++ landing_page/src/Component/Home/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Header.css';
+import background from '/home/gamal/ROME_1/landing_page/src/assets/bg.jpg';
 
 const Header = () => {
     return (
-        <header>
-            <div className="container">
+        <header >
+            <div className="container" style={{ backgroundImage: `url(${background})` }}>
                 <div className="row">
                     <div className="col-lg-12">
                         <h2> A chatbot for you when you need a friend!</h2>
