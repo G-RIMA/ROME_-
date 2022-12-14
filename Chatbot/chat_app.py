b1f3a207c9d7fe9671e8bd4ddd4b206e7f81430d
grima <gikandimaria178@gmail.com>
2022-12-14 12:02:02 +0300

    fixed some errors

--- Chatbot/chat_app.py
+++ Chatbot/chat_app.py
@@ -2,16 +2,15 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
 # Creating ChatBot Instance
 chatbot = ChatBot(
     'Rome',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'I am sorry, but I do not understand. I am still learning.',
-            'maximum_similarity_threshold': 0.90
-        }
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
     ],
     database_uri='sqlite:///database.sqlite3'
     
